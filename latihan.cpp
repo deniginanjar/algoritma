7ecda241a744e24167f15d48193028e6220ff917
deniginanjar <deginsundanese@gmail.com>
2017-03-23 20:45:42 +0700

    Add files via upload

---
+++ latihan.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int x;
+	cout << "Masukkan nilai X : ";
+	cin >> x;
+	cout << "nilai yang kamu masukan adalah :" ; cout << x;
+	return 0;
+}
